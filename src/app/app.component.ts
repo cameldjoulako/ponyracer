618de616b4f7803ca1b2c6141fedfcdf690a9672
Camel Djoulako <cameldjoulako17@gmail.com>
2024-12-19 12:43:44 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pr-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ponyracer';
+}
